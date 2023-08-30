52d24cd2b2717390c21adc20b9f8803552f102c1
fabriciocarvalho07 <fabricio.fbotg@gmail.com>
2023-08-30 10:57:43 -0300

    meu primeiro commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function trocacor(cor) {
+
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+
+
+}
+function trocaimagem (imagem){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem  }
